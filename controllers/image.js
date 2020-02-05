da7ac2d350b6e95074b274a8daee49c1e3f957d7
hayesm92 <46799216+hayesm92@users.noreply.github.com>
2020-02-04 20:45:18 -0500

    im trying

--- controllers/image.js
+++ controllers/image.js
@@ -18,7 +18,7 @@ const handleImage = (req,res, db) => {
       .increment('entries', 1)
       .returning('entries')
       .then(entries => {
-          res.json(entries);
+          res.json(entries[0]);
       })
       .catch(err => 
         res.status(400).json('unable to get entries'))
