5a34da5e05aa634e5424343464b1d25c1d8b5d16
hayesm92 <46799216+hayesm92@users.noreply.github.com>
2020-02-05 20:20:26 -0500

    image

--- controllers/image.js
+++ controllers/image.js
@@ -14,7 +14,7 @@ const handleApiCall = (req,res) =>{
 }
 const handleImage = (req,res, db) => {
     const {id} = req.body;
-    db('users').where('id', '=', id)
+    return db('users').where('id', '=', id)
       .increment('entries', 1)
       .returning('entries')
       .then(entries => {
